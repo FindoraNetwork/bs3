619333c2926fa31502b3fd401d5019f6ae9e4466
tiannian <dtiannian@aliyun.com>
2021-08-25 02:41:47 +0800

    remove sled testing

--- src/backend/sled.rs
+++ src/backend/sled.rs
@@ -42,59 +42,59 @@ impl SledBackend {
 // }
 // }
 
-#[cfg(test)]
-mod tests {
-    use core::cell::RefCell;
-    use std::println;
-
-    use alloc::{collections::BTreeMap, sync::Arc, vec, vec::Vec};
-
-    use crate::Result;
-
-    pub type MemTree = BTreeMap<Vec<u8>, Vec<u8>>;
-
-    pub struct TestBackend {
-        pub cache: Arc<MemTree>,
-        rc_ptr: RefCell<Arc<MemTree>>,
-    }
-
-    impl TestBackend {
-        pub fn new() -> Self {
-            let cache = Arc::new(BTreeMap::new());
-
-            let rc_ptr = RefCell::new(cache.clone());
-            Self { cache, rc_ptr }
-        }
-
-        pub fn get(&self, key: &[u8]) -> Result<Option<&[u8]>> {
-            let value = vec![1, 2, 3];
-
-            let has_key = self.cache.contains_key(key);
-            println!("size?: {}", Arc::strong_count(&self.cache));
-
-            if !has_key {
-                let mut rc_ptr = self.rc_ptr.borrow_mut();
-                println!("size?: {}", Arc::strong_count(&rc_ptr));
-                let cache_mut = Arc::get_mut(&mut rc_ptr).unwrap();
-                cache_mut.insert(Vec::from(key), value);
-            }
-
-            // let cache = self.cache.upgrade().unwrap();
-            // Ok(cache.get(key).map(|v| v.as_slice()))
-            Ok(None)
-        }
-    }
-
-    #[test]
-    fn test_arc() {
-        let testing = TestBackend::new();
-
-        let key = vec![1, 2, 3];
-
-        println!("result is: {:?}", testing.get(&key));
-
-        println!("inner is: {:?}", testing.cache);
-
-        println!("result is: {:?}", testing.get(&key));
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use core::cell::RefCell;
+//     use std::println;
+//
+//     use alloc::{collections::BTreeMap, sync::Arc, vec, vec::Vec};
+//
+//     use crate::Result;
+//
+//     pub type MemTree = BTreeMap<Vec<u8>, Vec<u8>>;
+//
+//     pub struct TestBackend {
+//         pub cache: Arc<MemTree>,
+//         rc_ptr: RefCell<Arc<MemTree>>,
+//     }
+//
+//     impl TestBackend {
+//         pub fn new() -> Self {
+//             let cache = Arc::new(BTreeMap::new());
+//
+//             let rc_ptr = RefCell::new(cache.clone());
+//             Self { cache, rc_ptr }
+//         }
+//
+//         pub fn get(&self, key: &[u8]) -> Result<Option<&[u8]>> {
+//             let value = vec![1, 2, 3];
+//
+//             let has_key = self.cache.contains_key(key);
+//             println!("size?: {}", Arc::strong_count(&self.cache));
+//
+//             if !has_key {
+//                 let mut rc_ptr = self.rc_ptr.borrow_mut();
+//                 println!("size?: {}", Arc::strong_count(&rc_ptr));
+//                 let cache_mut = Arc::get_mut(&mut rc_ptr).unwrap();
+//                 cache_mut.insert(Vec::from(key), value);
+//             }
+//
+//             // let cache = self.cache.upgrade().unwrap();
+//             // Ok(cache.get(key).map(|v| v.as_slice()))
+//             Ok(None)
+//         }
+//     }
+//
+//     #[test]
+//     fn test_arc() {
+//         let testing = TestBackend::new();
+//
+//         let key = vec![1, 2, 3];
+//
+//         println!("result is: {:?}", testing.get(&key));
+//
+//         println!("inner is: {:?}", testing.cache);
+//
+//         println!("result is: {:?}", testing.get(&key));
+//     }
+// }
